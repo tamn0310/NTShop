49478bcded2fa8cc7ba04c22e66a2322a8798d67
KaiKai <tamn0310@gmail.com>
2019-02-23 10:57:49 +0700

    add entity framework code first for date

---
+++ NTShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NTShop.Data.Infrastructure
+{
+    public interface IDbFactory : IDisposable
+    {
+        NTShopDbContext Init();
+    }
+}
---
+++ NTShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿namespace NTShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private NTShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public NTShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
