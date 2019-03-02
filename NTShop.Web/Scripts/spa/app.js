2c424c62e40434cf8e1dbec433e9f91304731bb6
KaiKai <tamn0310@gmail.com>
2019-03-02 10:07:59 +0700

    what is scope and rootscope?

---
+++ NTShop.Web/Scripts/spa/app.js
@@ -0,0 +1,17 @@
+﻿
+var myApp = angular.module('myModule', []);
+
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+//declare
+function schoolController($scope) {
+    $scope.message = "Announcement from school";
+}
+function studentController($scope) {
+    $scope.message = "This is my message from student";
+}
+function teacherController($scope) {
+    $scope.message = "This is my message from teacher";
+}
