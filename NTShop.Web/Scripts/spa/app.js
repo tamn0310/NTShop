af47ac977c070b7a6e73dec6ac69df623f34d9e1
KaiKai <tamn0310@gmail.com>
2019-03-02 21:29:23 +0700

    service in angularjs

--- NTShop.Web/Scripts/spa/app.js
+++ NTShop.Web/Scripts/spa/app.js
@@ -2,16 +2,32 @@
 var myApp = angular.module('myModule', []);
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service('Validator',Validator);
+
+schoolController.$inject = ['$scope', 'Validator'];
+
 
 //declare
-function schoolController($scope) {
-    $scope.message = "Announcement from school";
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+
 }
-function studentController($scope) {
-    $scope.message = "This is my message from student";
+
+function Validator() {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "This is even";
+        }
+        else 
+            return "This is odd";
+        
+    }
+
 }
-function teacherController($scope) {
-    $scope.message = "This is my message from teacher";
-}
+
