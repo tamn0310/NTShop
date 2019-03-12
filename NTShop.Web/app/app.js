8177ad0dad28124975dc7e3c207f32700b8832a8
KaiKai <tamn0310@gmail.com>
2019-03-12 20:07:22 +0700

    route implement for app

--- NTShop.Web/app/app.js
+++ NTShop.Web/app/app.js
@@ -1,1 +1,14 @@
-﻿
+﻿(function () {
+    angular.module('ntshop', ['ntshop.products','ntshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller:"homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ NTShop.Web/app/components/products/products.module.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular.module('ntshop.products', ['ntshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('products_add', {
+            url: "/products_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
